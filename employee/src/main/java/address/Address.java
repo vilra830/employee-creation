0cc36a062a41f98694c9b5929fb2f2136b75ff2a
vilra830 <villafuerte.rhea@gmail.com>
2025-03-18 15:14:37 +1100

    adds spring project as first commit

---
+++ employee/src/main/java/address/Address.java
@@ -0,0 +1,14 @@
+package address;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Address {
+
+
+private
+
+
+}
---
+++ employee/src/main/java/employee/Employee.java
@@ -0,0 +1,143 @@
+package employee;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table()
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    @ManyToAny
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    
+    @Enumerated(EnumType.STRING)
+    private WorkSchedule workSchedule;
+
+    @Enumerated(EnumType.STRING)
+    private ContractType contracType;
+
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public WorkSchedule getWorkSchedule() {
+        return workSchedule;
+    }
+
+    public void setWorkSchedule(WorkSchedule workSchedule) {
+        this.workSchedule = workSchedule;
+    }
+
+    public ContractType getContracType() {
+        return contracType;
+    }
+
+    public void setContracType(ContractType contracType) {
+        this.contracType = contracType;
+    }
+
+    public enum ContractType {
+
+        PERMANENT, 
+        CONTRACT
+    }
+
+    public enum WorkSchedule {
+        FULL_TIME,
+        PART_TIME
+    }
+
+
+
+
+
+
+}
